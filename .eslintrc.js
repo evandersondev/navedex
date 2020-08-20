31ff7f4e847cfb3d54a64c5322088063123b79e3
evandersondev <evandersonvasconcelos23@gmail.com>
2020-08-20 11:19:39 -0300

    create initial structure, adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    camelcase: 'off',
+    'no-undef': 'off',
+  },
+}
